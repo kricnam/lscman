ebf1975689e32f28037768d38f06578b53d39ab0
me.xinxin <me.xinxin@8e5c3647-7598-2970-c077-f6edb9073431>
2010-11-13 04:26:50 +0000

    add read csv file support

---
+++ trunk/src/DataFile.h
@@ -0,0 +1,30 @@
+// DataFile.h: interface for the CDataFile class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_DATAFILE_H__3AF7DAD3_0CA6_4931_A8EB_991ED79F3EE1__INCLUDED_)
+#define AFX_DATAFILE_H__3AF7DAD3_0CA6_4931_A8EB_991ED79F3EE1__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CDataFile  
+{
+public:
+	bool IsValidFile(void);
+	int Open(void);
+	int Open(LPCTSTR szFileName);
+	int GetSpectrumData(int* pArray,int len);
+	
+	CDataFile();
+	virtual ~CDataFile();
+protected:
+	int chopFirstCSVField(CString& strLine,CString& strField);
+	int readLine(CString& strLine);
+	bool gotoSpetrumStart(void);
+    CString strFileName;
+	FILE* fData;
+};
+
+#endif // !defined(AFX_DATAFILE_H__3AF7DAD3_0CA6_4931_A8EB_991ED79F3EE1__INCLUDED_)
